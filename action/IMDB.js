12d845ff8b68457c82ecc865c5a87ae5ddfc664b
LanS0 <michael.alan00010@gmail.com>
2023-02-13 10:11:12 +0700

    commit : - .env - action IMDB

--- action/IMDB.js
+++ action/IMDB.js
@@ -1,4 +1,29 @@
 import axios from "axios"
 
 export default async function GetIMDB(){
+
+    try {
+        const options = {
+            method: 'GET',
+            url: 'https://imdb-top-100-movies.p.rapidapi.com/',
+            headers: {
+                'X-RapidAPI-Key': process.env.X-RapidAPI-Key,
+                'X-RapidAPI-Host': process.env.X-RapidAPI-Host
+            }
+        };
+
+        let result = await axios.get(
+            "https://imdb-top-100-movies.p.rapidapi.com/",
+            options
+        );
+
+        if(result){
+           return result
+        }
+
+    } catch (error) {
+        console.log("ERROR :", error)
+        return error
+    }
+
 }
